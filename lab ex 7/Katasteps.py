59760188b4006ad12ac7fb36a2f6f630ea6b8aef
Viktoria Inga <viktoriainga@gmail.com>
2020-03-11 13:26:50 +0000

    Add supports single digit

--- lab ex 7/Katasteps.py
+++ lab ex 7/Katasteps.py
@@ -1,3 +1,5 @@
 def add(numbers):
     if numbers == "":
         return 0
+    
+    
--- lab ex 7/katastepts_test.py
+++ lab ex 7/katastepts_test.py
@@ -2,3 +2,6 @@ import Katasteps
 def test_on_empty_string():
     assert Katasteps.add("") == 0
 
+def test_on_number():
+    assert Katasteps.add(5) == 5
+
